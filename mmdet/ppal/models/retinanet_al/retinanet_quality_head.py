e107b019ac60e85dc83dc28e21b47cd53b300fc0
Brym-Gyimah <50814684+Brym-Gyimah@users.noreply.github.com>
2023-03-06 14:10:11 -0500

    Update retinanet_quality_head.py

--- mmdet/ppal/models/retinanet_al/retinanet_quality_head.py
+++ mmdet/ppal/models/retinanet_al/retinanet_quality_head.py
@@ -5,67 +5,58 @@ from mmcv.cnn import ConvModule
 from mmcv.runner import force_fp32
 from mmdet.core import (anchor_inside_flags, build_assigner, build_bbox_coder,
                         build_prior_generator, build_sampler, images_to_levels,
-                        multi_apply, reduce_mean, unmap)
+                        multi_apply, unmap)
 
-from mmdet.models.builder import HEADS, build_loss
-from mmdet.models.dense_heads.atss_head import ATSSHead
-
+from mmdet.models.builder import HEADS
+from mmdet.models.dense_heads.retina_head import RetinaHead
 from mmdet.core.bbox.iou_calculators.iou2d_calculator import bbox_overlaps
 
 from mmdet.ppal.models.utils import concat_all_sum
-INF = 1e8
 
 @HEADS.register_module()
-class ATSSQualityOTAHead(ATSSHead):
+class RetinaQualityEMAHead(RetinaHead):
     def __init__(self,
                  num_classes,
                  in_channels,
                  stacked_convs=4,
                  conv_cfg=None,
-                 norm_cfg=dict(type='GN', num_groups=32, requires_grad=True),
-                 reg_decoded_bbox=True,
-                 loss_centerness=dict(
-                     type='CrossEntropyLoss',
-                     use_sigmoid=True,
-                     loss_weight=1.0),
+                 norm_cfg=None,
+                 anchor_generator=dict(
+                     type='AnchorGenerator',
+                     octave_base_scale=4,
+                     scales_per_octave=3,
+                     ratios=[0.5, 1.0, 2.0],
+                     strides=[8, 16, 32, 64, 128]),
                  init_cfg=dict(
                      type='Normal',
                      layer='Conv2d',
                      std=0.01,
                      override=dict(
                          type='Normal',
-                         name='atss_cls',
+                         name='retina_cls',
                          std=0.01,
                          bias_prob=0.01)),
-                # base_momentum=0.999 update this to OTA variables
-                # quality_xi=0.6 update this to OTA variables
+                 base_momentum=0.999,
+                 quality_xi=0.6,
                  **kwargs):
         self.stacked_convs = stacked_convs
         self.conv_cfg = conv_cfg
-        self.norm_cfg = norm_cfg      
-        super(ATSSHead, self).__init__(
+        self.norm_cfg = norm_cfg
+        super(RetinaHead, self).__init__(
             num_classes,
             in_channels,
-            reg_decoded_bbox=reg_decoded_bbox,
+            anchor_generator=anchor_generator,
             init_cfg=init_cfg,
             **kwargs)
-        self.sampling = False
-        if self.train_cfg:
-            self.assigner = build_assigner(self.train_cfg.assigner)
-            # SSD sampling=False so use PseudoSampler
-            sampler_cfg = dict(type='PseudoSampler')
-            self.sampler = build_sampler(sampler_cfg, context=self)
-        self.loss_centerness = build_loss(loss_centerness)
-                     
-       # self.quality_xi = quality_xi update this to OTA variables
-       # self.register_buffer('class_momentum', torch.ones((num_classes,)) * base_momentum)
-       # self.register_buffer('class_quality', torch.zeros((num_classes,)))
-       # self.base_momentum = base_momentum update these to OTA variables
+        self.quality_xi = quality_xi
+
+        self.register_buffer('class_momentum', torch.ones((num_classes,)) * base_momentum)
+        self.register_buffer('class_quality', torch.zeros((num_classes,)))
+        self.base_momentum = base_momentum
 
 
-    
-    def loss_single(self, anchors, cls_score, bbox_pred, centerness, labels,
-                    label_weights, bbox_targets, num_total_samples):
+    def loss_single(self, cls_score, bbox_pred, anchors, labels, label_weights,
+                    bbox_targets, bbox_weights, num_total_samples):
         """Compute loss of a single scale level.
         Args:
             cls_score (Tensor): Box scores for each scale level
@@ -80,66 +71,57 @@ class ATSSQualityOTAHead(ATSSHead):
                 (N, num_total_anchors)
             bbox_targets (Tensor): BBox regression targets of each anchor
                 weight shape (N, num_total_anchors, 4).
-            num_total_samples (int): Number os positive samples that is
-                reduced over all GPUs.
+            bbox_weights (Tensor): BBox regression loss weights of each anchor
+                with shape (N, num_total_anchors, 4).
+            num_total_samples (int): If sampling, num total samples equal to
+                the number of total anchors; Otherwise, it is the number of
+                positive anchors.
         Returns:
             dict[str, Tensor]: A dictionary of loss components.
         """
-
-        anchors = anchors.reshape(-1, 4)
-        cls_score = cls_score.permute(0, 2, 3, 1).reshape(
-            -1, self.cls_out_channels).contiguous()
-        bbox_pred = bbox_pred.permute(0, 2, 3, 1).reshape(-1, 4)
-        centerness = centerness.permute(0, 2, 3, 1).reshape(-1)
-        bbox_targets = bbox_targets.reshape(-1, 4)
+        # classification loss
         labels = labels.reshape(-1)
         label_weights = label_weights.reshape(-1)
-
-        # classification loss
+        cls_score = cls_score.permute(0, 2, 3,
+                                      1).reshape(-1, self.cls_out_channels)
         loss_cls = self.loss_cls(
             cls_score, labels, label_weights, avg_factor=num_total_samples)
 
-        # FG cat_id: [0, num_classes -1], BG cat_id: num_classes
-        bg_class_ind = self.num_classes
-        pos_inds = ((labels >= 0)
-                    & (labels < bg_class_ind)).nonzero().squeeze(1)
-
-        if len(pos_inds) > 0:
-            pos_bbox_targets = bbox_targets[pos_inds]
-            pos_bbox_pred = bbox_pred[pos_inds]
-            pos_anchors = anchors[pos_inds]
-            pos_centerness = centerness[pos_inds]
-
-            centerness_targets = self.centerness_target(
-                pos_anchors, pos_bbox_targets)
-            pos_decode_bbox_pred = self.bbox_coder.decode(
-                pos_anchors, pos_bbox_pred)
-
-            # regression loss
-            loss_bbox = self.loss_bbox(
-                pos_decode_bbox_pred,
-                pos_bbox_targets,
-                weight=centerness_targets,
-                avg_factor=1.0)
-
-            # centerness loss
-            loss_centerness = self.loss_centerness(
-                pos_centerness,
-                centerness_targets,
-                avg_factor=num_total_samples)
+        # regression loss
+        bbox_targets = bbox_targets.reshape(-1, 4)
+        bbox_weights = bbox_weights.reshape(-1, 4)
+        bbox_pred = bbox_pred.permute(0, 2, 3, 1).reshape(-1, 4)
+        if self.reg_decoded_bbox:
+            # When the regression loss (e.g. `IouLoss`, `GIouLoss`)
+            # is applied directly on the decoded bounding boxes, it
+            # decodes the already encoded coordinates to absolute format.
+            anchors = anchors.reshape(-1, 4)
+            bbox_pred = self.bbox_coder.decode(anchors, bbox_pred)
 
         else:
-            loss_bbox = bbox_pred.sum() * 0
-            loss_centerness = centerness.sum() * 0
-            centerness_targets = bbox_targets.new_tensor(0.)
+            with torch.no_grad():
+                _anchors = anchors.reshape(-1, 4)
+                _bbox_pred = self.bbox_coder.decode(_anchors, bbox_pred)
+                _bbox_gt = self.bbox_coder.decode(_anchors, bbox_targets)
 
-        return loss_cls, loss_bbox, loss_centerness, centerness_targets.sum()
+                valid_inds = labels < self.num_classes
+                _labels = labels[valid_inds]
+                iou = bbox_overlaps(_bbox_pred[valid_inds], _bbox_gt[valid_inds], is_aligned=True)
+                p = torch.sigmoid(cls_score[valid_inds])[torch.arange(iou.shape[0], dtype=torch.long).to(device=iou.device), _labels]
+                quality = torch.pow(p, self.quality_xi) * torch.pow(iou, 1. - self.quality_xi)
+                classwise_quality = torch.stack((_labels, quality), dim=-1)
 
-    @force_fp32(apply_to=('cls_scores', 'bbox_preds', 'centernesses'))
+        loss_bbox = self.loss_bbox(
+            bbox_pred,
+            bbox_targets,
+            bbox_weights,
+            avg_factor=num_total_samples)
+        return loss_cls, loss_bbox, classwise_quality
+
+    @force_fp32(apply_to=('cls_scores', 'bbox_preds'))
     def loss(self,
              cls_scores,
              bbox_preds,
-             centernesses,
              gt_bboxes,
              gt_labels,
              img_metas,
@@ -150,15 +132,13 @@ class ATSSQualityOTAHead(ATSSHead):
                 Has shape (N, num_anchors * num_classes, H, W)
             bbox_preds (list[Tensor]): Box energies / deltas for each scale
                 level with shape (N, num_anchors * 4, H, W)
-            centernesses (list[Tensor]): Centerness for each scale
-                level with shape (N, num_anchors * 1, H, W)
             gt_bboxes (list[Tensor]): Ground truth bboxes for each image with
                 shape (num_gts, 4) in [tl_x, tl_y, br_x, br_y] format.
             gt_labels (list[Tensor]): class indices corresponding to each box
             img_metas (list[dict]): Meta information of each image, e.g.,
                 image size, scaling factor, etc.
-            gt_bboxes_ignore (list[Tensor] | None): specify which bounding
-                boxes can be ignored when computing the loss.
+            gt_bboxes_ignore (None | list[Tensor]): specify which bounding
+                boxes can be ignored when computing the loss. Default: None
         Returns:
             dict[str, Tensor]: A dictionary of loss components.
         """
@@ -166,10 +146,10 @@ class ATSSQualityOTAHead(ATSSHead):
         assert len(featmap_sizes) == self.prior_generator.num_levels
 
         device = cls_scores[0].device
+
         anchor_list, valid_flag_list = self.get_anchors(
             featmap_sizes, img_metas, device=device)
         label_channels = self.cls_out_channels if self.use_sigmoid_cls else 1
-
         cls_reg_targets = self.get_targets(
             anchor_list,
             valid_flag_list,
@@ -180,31 +160,51 @@ class ATSSQualityOTAHead(ATSSHead):
             label_channels=label_channels)
         if cls_reg_targets is None:
             return None
+        (labels_list, label_weights_list, bbox_targets_list, bbox_weights_list,
+         num_total_pos, num_total_neg) = cls_reg_targets
+        num_total_samples = (
+            num_total_pos + num_total_neg if self.sampling else num_total_pos)
 
-        (anchor_list, labels_list, label_weights_list, bbox_targets_list,
-         bbox_weights_list, num_total_pos, num_total_neg) = cls_reg_targets
+        # anchor number of multi levels
+        num_level_anchors = [anchors.size(0) for anchors in anchor_list[0]]
+        # concat all level anchors and flags to a single tensor
+        concat_anchor_list = []
+        for i in range(len(anchor_list)):
+            concat_anchor_list.append(torch.cat(anchor_list[i]))
+        all_anchor_list = images_to_levels(concat_anchor_list,
+                                           num_level_anchors)
 
-        num_total_samples = reduce_mean(
-            torch.tensor(num_total_pos, dtype=torch.float,
-                         device=device)).item()
-        num_total_samples = max(num_total_samples, 1.0)
+        losses_cls, losses_bbox, classwise_quality = multi_apply(
+            self.loss_single,
+            cls_scores,
+            bbox_preds,
+            all_anchor_list,
+            labels_list,
+            label_weights_list,
+            bbox_targets_list,
+            bbox_weights_list,
+            num_total_samples=num_total_samples)
 
-        losses_cls, losses_bbox, loss_centerness,\
-            bbox_avg_factor = multi_apply(
-                self.loss_single,
-                anchor_list,
-                cls_scores,
-                bbox_preds,
-                centernesses,
-                labels_list,
-                label_weights_list,
-                bbox_targets_list,
-                num_total_samples=num_total_samples)
+        with torch.no_grad():
+            classwise_quality = torch.cat(classwise_quality, dim=0)
+            _classes = classwise_quality[:, 0]
+            _qualities = classwise_quality[:, 1]
 
-        bbox_avg_factor = sum(bbox_avg_factor)
-        bbox_avg_factor = reduce_mean(bbox_avg_factor).clamp_(min=1).item()
-        losses_bbox = list(map(lambda x: x / bbox_avg_factor, losses_bbox))
-        return dict(
-            loss_cls=losses_cls,
-            loss_bbox=losses_bbox,
-            loss_centerness=loss_centerness)
+            collected_counts = classwise_quality.new_full((self.num_classes,), 0)
+            collected_qualities = classwise_quality.new_full((self.num_classes,), 0)
+            for i in range(self.num_classes):
+                cq = _qualities[_classes == i]
+                if cq.numel() > 0:
+                    collected_counts[i] += torch.ones_like(cq).sum()
+                    collected_qualities[i] += cq.sum()
+            collected_counts = concat_all_sum(collected_counts)
+            collected_qualities = concat_all_sum(collected_qualities)
+            avg_qualities = collected_qualities / (collected_counts + 1e-5)
+            self.class_quality = self.class_momentum * self.class_quality + \
+                                 (1. - self.class_momentum) * avg_qualities
+            self.class_momentum = torch.where(
+                avg_qualities > 0,
+                torch.zeros_like(self.class_momentum) + self.base_momentum,
+                self.class_momentum * self.base_momentum)
+
+        return dict(loss_cls=losses_cls, loss_bbox=losses_bbox)
