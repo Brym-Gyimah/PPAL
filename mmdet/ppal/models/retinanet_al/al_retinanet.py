8249cdfb56bd30c2d426d8bbfee097a0e354d879
Brym-Gyimah <50814684+Brym-Gyimah@users.noreply.github.com>
2023-03-06 14:11:21 -0500

    Update al_retinanet.py

--- mmdet/ppal/models/retinanet_al/al_retinanet.py
+++ mmdet/ppal/models/retinanet_al/al_retinanet.py
@@ -2,15 +2,15 @@ import torch
 import numpy as np
 
 from mmdet.models.builder import DETECTORS
-from mmdet.models.detectors.fcos import FCOS
+from mmdet.models.detectors.retinanet import RetinaNet
 from mmdet.ppal.models.utils import bbox2result_with_uncertainty
 
 @DETECTORS.register_module()
-class ALFCOS(FCOS):
+class ALRetinaNet(RetinaNet):
 
     def __init__(self,
                  **kwargs):
-        super(ALFCOS, self).__init__(**kwargs)
+        super(ALRetinaNet, self).__init__(**kwargs)
 
     def extract_feat(self, img):
         """Directly extract features from the backbone+neck."""
@@ -38,5 +38,3 @@ class ALFCOS(FCOS):
             for det_bboxes, det_labels, cls_uncertainties, box_uncertainties in results_list
         ]
         return bbox_results
-
-
